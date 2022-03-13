88509a5650a01ba25e012a8a676bc207f2d145c7
Aron Sigurdsson-Morris <aron@airgraft.com>
2022-03-12 21:36:20 -0500

    init

--- scripts/module.js
+++ scripts/module.js
@@ -3,5 +3,11 @@ Hooks.once('init', async function() {
 });
 
 Hooks.once('ready', async function() {
-
+    console.log("Augmented Initiative | Ready!")
 });
+
+class AugmentedInitiative {
+    static TEMPLATES = {
+        TODOLIST: `modules/${this.ID}/templates/initiative.hbs`
+      }
+}
