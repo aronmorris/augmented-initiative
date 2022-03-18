663ed22da3136b30d6c4e95e5ee82a550c8938bd
Aron Sigurdsson-Morris <aron@airgraft.com>
2022-03-18 15:54:06 -0400

    Final cleanup

--- scripts/module.js
+++ scripts/module.js
@@ -2,22 +2,23 @@ Hooks.once('ready', async function () {
     console.log("Augmented Initiative | Ready!")
 });
 
-Hooks.on("updateActor", async (actor, data, options, userId) => {
-    if (!game.user?.isGM) return;
-    console.log("Augmented Initiative caught this updateActor event");
-    if (game.combat && game.combat.isActive) {
-        console.log("Ok, this works");
-    }
+Hooks.on("updateActor", async () => {
+    ui.combat.render();
 })
 
-Hooks.on("renderCombatTracker", async (app, html, data) => {
+Hooks.on("renderCombatTracker", async (app, html) => {
     if (!game.user?.isGM) return;
 
     let combatantsDOM = html[0].querySelectorAll("li.combatant");
     combatantsDOM.forEach(el => {
         const actorData = getCombatantActorData(el.dataset.combatantId);
         el.querySelector("div.token-initiative").before(
-            $.parseHTML(getReadyHtml(actorData.hp, actorData.structure, actorData.heat, actorData.stress))[0]
+            $.parseHTML(getReadyHtml(
+                actorData.hp ?? 0,
+                actorData.structure ?? 0,
+                actorData.heat ?? 0,
+                actorData.stress ?? 0)
+            )[0]
         );
     });
 });
@@ -27,7 +28,7 @@ function getCombatantActorData(combatantId) {
 }
 
 function getReadyHtml(hp, structure, heat, stress) {
-    return `<div class="token-stats flex-center">
+    return `<div class="token-stats flexrow">
                 <div class="token-stats-hp">
                     <i class="token-stats-text  mdi mdi-heart-outline"></i>
                     <span class="token-stats-text">${hp}</span>
@@ -46,15 +47,3 @@ function getReadyHtml(hp, structure, heat, stress) {
                 </div>
             </div>`;
 }
-
-//Need to fetch the data for each combatant in the combat tracker, then display it next to the clickable
-//then we can worry about making it a formapplication or whatever
-
-//So step 1:
-//Identify each initiative element so we can iter over them CHECK
-
-//Step 2:
-//Inject character data in there CHECK, WE GOT DATA
-
-//Step 3:
-//Make the field pretty. We need FONTS and to know WHERE THE ICONS COME FROM. Ask Eranziel.
